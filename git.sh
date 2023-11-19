3312e3a14a8c8e3bc2d074353cf954eff5d942d9
vic <vic@psp-power.com.tw>
2023-11-19 20:44:18 +0800

    finish git shortcut

--- git.sh
+++ git.sh
@@ -14,16 +14,18 @@ function gm2() {
     git pull origin "$target_branch" --rebase
     git merge "$current_branch"
     echo "merge $current_branch to $target_branch"
-
 }
 
-# gpush git pull --rebase then push
-function gpullpush() {
+# gcommitpush git pull --rebase then push
+function gcommitpush() {
+    git add .
+    git commit -m $1
     current_branch=$(git rev-parse --abbrev-ref HEAD)
-    git pull origin "$current_branch" --rebase && git push
+    git pull origin "$current_branch" --rebase && git push origin head
 }
 
 
+# grem remove branch which has been merged to xx branch
 function grem() {
     git branch --merged $1 | egrep -v "(^\*|$1)" | xargs git branch -d
 }
